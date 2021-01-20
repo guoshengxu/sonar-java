8c332a3a53411d311110c9bfe8709497ce9dc345
Dorian Burihabwa <dorian.burihabwa@sonarsource.com>
2021-01-20 10:24:04 +0100

    SONARJAVA-3480 S5917: Detect DatetimeFormatter mismatches

--- java-checks/src/main/java/org/sonar/java/checks/CheckList.java
+++ java-checks/src/main/java/org/sonar/java/checks/CheckList.java
@@ -358,6 +358,7 @@ public final class CheckList {
       DataHashingCheck.class,
       DateAndTimesCheck.class,
       DateFormatWeekYearCheck.class,
+      DateTimeFormatterMismatchCheck.class,
       DateUtilsTruncateCheck.class,
       DeadStoreCheck.class,
       DebugFeatureEnabledCheck.class,
